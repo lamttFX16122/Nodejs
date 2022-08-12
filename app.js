ac280a2a1627b79f1d23316fb3db071bf2972cdc
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-08-12 15:21:05 +0700

    directional request from url / to url /message

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if (url === '/') {
+        var str_f = '<html>';
+        str_f += '<header>';
+        str_f += '<title>Enter Message</title>';
+        str_f += '</header>';
+        str_f += '<body>';
+        str_f += '<form action="/message" method="POST">';
+        str_f += '<input type="text" name="message">';
+        str_f += '<button type="submit">Send</button>';
+        str_f += '</form>';
+        str_f += '</body>';
+        str_f += '</html>';
+        res.write(str_f);
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     //html
     var str = '<html>';
