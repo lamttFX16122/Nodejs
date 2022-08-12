1df165d19f36144eccceecc08563cc2e21f0f64a
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-08-12 15:00:51 +0700

    log req.url, req.method, req.header of request and send response html for client by response

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    //html
+    var str = '<html>';
+    str += '<header>';
+    str += '<title>My First Page</title>';
+    str += '</header>';
+    str += '<body>';
+    str += '<h1>Hello from my Nodejs Server</h1>';
+    str += '</body>';
+    str += '</html>';
+    res.write(str);
+    res.end();
 })
 
 server.listen(8099);
