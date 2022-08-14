776ccd37e266c663ac0c06a45dcdf7eeec03d786
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-08-14 15:39:38 +0700

    in the 2th Middleware return a paragraph HTML with content Hello from Express.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use((req, res, next) => {
 })
 app.use((req, res, next) => {
     console.log("In the second Middleware!");
+    res.send('<h1>Hello from Express.js</h1>');
 })
 const server = http.createServer(app);
 server.listen(8099);
