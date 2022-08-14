0f5749e4e35229aa161bdd69cd6769d538f88615
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-08-14 15:29:39 +0700

    register 2 middleware and print console log for each middleware

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log("In the Middleware");
+    next();
+})
+app.use((req, res, next) => {
+    console.log("In the second Middleware!");
+})
 const server = http.createServer(app);
 server.listen(8099);
