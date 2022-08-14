5b95ac8bc402bb7240863cfb72ab3c44003dd123
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-08-14 15:55:00 +0700

    add 3 middleware (the first away run, the second return html add product and path add-product and the last return html Hello from Express.js)

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the Middleware");
+app.use('/', (req, res, next) => {
+    console.log('This away run');
     next();
 })
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log("In the second Middleware!");
+    res.send('<h1>The add Product Page</h1>');
+})
+app.use('/', (req, res, next) => {
     console.log("In the second Middleware!");
     res.send('<h1>Hello from Express.js</h1>');
 })
