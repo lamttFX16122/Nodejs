cc80eb69dd6169173b04f555d89365960bddc116
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-08-16 22:41:28 +0700

    add middleware in app.js for wrong paths

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRouter);
 app.use(shopRouter);
 
+app.use((req, res) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 const server = http.createServer(app);
 server.listen(8099);
