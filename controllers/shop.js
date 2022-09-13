a8eec10e6232d198cbf07b73de7dd1e386e8b65e
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-09-13 11:49:55 +0700

    Adding a cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart = require('../models/cart');
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
         res.render('shop/product-list', {
@@ -40,7 +40,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const id = req.body.productId;
-    console.log(id);
+    Product.findById(id, product => {
+        Cart.addProduct(id, product.price);
+    })
     res.redirect('/cart');
 }
 
