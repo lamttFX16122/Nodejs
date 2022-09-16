39fa87857366b663c65ffc8be46639d87facaa21
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-09-16 16:31:12 +0700

    inserting data into the database

--- models/product.js
+++ models/product.js
@@ -12,7 +12,7 @@ module.exports = class Product {
     }
 
     save() {
-
+        return db.execute('insert into products(title, price, description, imageUrl) values(?,?,?,?)', [this.title, this.price, this.description, this.imageUrl]);
     }
     static deleteProduct(id) {
 
