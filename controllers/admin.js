1952a107fdeb75b670d35f4ecff4db70be9d0996
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-09-19 21:33:13 +0700

    Using Magic Association Methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,20 +16,14 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    // const product = new Product(null, title, imageUrl, description, price);
-    // product.save()
-    //     .then(() => {
-    //         res.redirect('/');
-    //     })
-    //     .catch(err => console.log(err));
-    Product.create({
+    req.user.createProduct({
         title,
         price,
         imageUrl,
         description
     }).then((result) => {
         console.log("Created Product");
-        res.redirect('/');
+        res.redirect('/admin/products');
     }).catch(err => { console.log(err) });
 };
 
