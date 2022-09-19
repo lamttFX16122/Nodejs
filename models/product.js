b298e08f35a8f2c7e7c30399bc985bcace35377e
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-09-19 09:55:08 +0700

    defining a model

--- models/product.js
+++ models/product.js
@@ -1,26 +1,24 @@
-const Cart = require('./cart');
-const db = require('../util/database');
-
-
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
-
-    save() {
-        return db.execute('insert into products(title, price, description, imageUrl) values(?,?,?,?)', [this.title, this.price, this.description, this.imageUrl]);
-    }
-    static deleteProduct(id) {
-
-    }
-    static fetchAll() {
-        return db.execute('select * from products');
-    }
-    static findById(id) {
-        return db.execute('select * from products where products.id = ?', [id]);
-    }
-};
+});
+module.exports = Product;
