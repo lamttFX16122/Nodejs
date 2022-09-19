d36868dc511c9546f78bcb6c6fd8fd45de5e4168
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-09-19 09:38:18 +0700

    Connecting to the database

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '@717411N'
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: '@717411N'
+// });
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete', 'root', '@717411N', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
-module.exports = pool.promise();
+
+module.exports = sequelize;
