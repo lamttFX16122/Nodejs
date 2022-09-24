2df9cd7322624e792a8d9a5402759d0cb0c08546
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-09-24 13:59:45 +0700

    Installing the mongoDb driver

--- util/database.js
+++ util/database.js
@@ -6,11 +6,24 @@
 //     password: '@717411N'
 // });
 // module.exports = pool.promise();
+//=====================================
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('node-complete', 'root', '@717411N', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node-complete', 'root', '@717411N', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
+// module.exports = sequelize;
+//=====================================
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const url = 'mongodb+srv://thanhlam:thanhlam@cluster0.hatavqh.mongodb.net/?retryWrites=true&w=majority';
+const mongoConnect = cb => {
+    MongoClient.connect(url)
+        .then(connected => {
+            console.log('Connected');
+            cb(connected);
+        })
+        .catch(err => console.log(err));
+}
+module.exports = mongoConnect;
