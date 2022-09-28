164cf4b414480087f3efc578be60a4891cb17fa4
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-09-28 14:43:59 +0700

    Working on Cart Items & orders

--- models/user.js
+++ models/user.js
@@ -2,14 +2,27 @@ const mongoDb = require('mongodb');
 const ObjectId = mongoDb.ObjectId;
 const getDb = require('../util/database').getDb;
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.username = username;
         this.email = email;
+        this.cart = cart;
+        this._id = id;
     };
 
     save() {
         const db = getDb();
         return db.collection('users').insertOne(this);
+    }
+
+    addToCart(product) {
+        const updatedCart = {
+            items: [{
+                ...product,
+                quantity: 1
+            }]
+        };
+        const db = getDb();
+        return db.collection('users').updateOne({ _id: new ObjectId(this.id) }, { $set: { cart: updatedCart } });
     }
     static findUserById(id) {
         const db = getDb();
