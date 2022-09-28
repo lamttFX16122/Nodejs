95e4ad54e2ac26992a0d52e1500b23c8be729a41
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-09-28 16:57:28 +0700

    Displaying the cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,19 +39,16 @@ exports.getIndex = (req, res, next) => {
         .catch(err => console.log(err))
 };
 
-// exports.getCart = (req, res, next) => {
-//     req.user.getCart()
-//         .then(cart => {
-//             return cart.getProducts()
-//                 .then(product => {
-//                     res.render('shop/cart', {
-//                         path: '/cart',
-//                         pageTitle: 'Your Cart',
-//                         products: product
-//                     });
-//                 })
-//         }).catch(err => console.log(err));
-// };
+exports.getCart = (req, res, next) => {
+    req.user.getCart()
+        .then(products => {
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: products
+            });
+        }).catch(err => console.log(err));
+};
 
 exports.postCart = (req, res, next) => {
     const id = req.body.productId;
--- models/user.js
+++ models/user.js
@@ -36,6 +36,32 @@ class User {
         const db = getDb();
         return db.collection('users').updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });
     }
+
+    getCart() {
+        const db = getDb();
+        const productIds = this.cart.items.map(i => {
+            return i.productId;
+        })
+        return db.collection('products').find({ _id: { $in: productIds } })
+            .toArray()
+            .then(products => {
+
+                return products.map(j => {
+                    return {...j,
+                        quantity: this.cart.items.find(i => {
+                            return i.productId.toString() === j._id.toString();
+                        }).quantity
+                    }
+                })
+            })
+            // .then(result => {
+            //     console.log("=============================================================")
+            //     console.log('result', result)
+            // })
+            .catch(err => {
+                console.log(err);
+            })
+    }
     static findUserById(id) {
         const db = getDb();
         return db.collection('users').findOne({ _id: new ObjectId(id) });
