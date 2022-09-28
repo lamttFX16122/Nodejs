29cb1842d4ab1ff53fdfa388abdb1578677a62dd
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-09-28 15:50:19 +0700

    Storing multiple products in the cart

--- models/user.js
+++ models/user.js
@@ -15,11 +15,23 @@ class User {
     }
 
     addToCart(product) {
+        const cartProductIndex = this.cart.items.findIndex(value => {
+            return value.productId.toString() === product._id.toString();
+        });
+
+        let newQuantity = 1;
+        let newUpdatedCart = [...this.cart.items];
+        if (cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            newUpdatedCart[cartProductIndex].quantity = newQuantity;
+        } else {
+            newUpdatedCart.push({
+                productId: new ObjectId(product._id),
+                quantity: newQuantity
+            })
+        }
         let updatedCart = {
-            items: [{
-                productId: new ObjectId(product.id),
-                quantity: 1
-            }]
+            items: newUpdatedCart
         };
         const db = getDb();
         return db.collection('users').updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });
