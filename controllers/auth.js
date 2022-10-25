83ab169f3235301176abfef9d55f1aabca7ca12b
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-10-25 13:59:43 +0700

    Setting Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req.get('Cookie').split(':')[1].trim();
     return res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: isLoggedIn
     })
 }
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggedIn:true');
     res.redirect('/');
 }
