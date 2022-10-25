7bc0443d63ba125da5d389ca2899070112d3434c
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-10-25 22:12:41 +0700

    Two Tiny Improvements

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,5 @@
 const User = require('../models/user');
 exports.getLogin = (req, res, next) => {
-    //  const isLoggedIn = req.get('Cookie').split(':')[1].trim();
-    console.log(req.session)
     return res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
@@ -13,7 +11,10 @@ exports.postLogin = (req, res, next) => {
         .then(user => {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            res.redirect('/');
+            req.session.save((err) => {
+                console.log(err);
+                res.redirect('/');
+            })
         })
         .catch(err => console.log(err));
 }
