711c35062ee26b48adfde5dabc27ab64a8822ace
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-10-25 14:16:49 +0700

    Configuring Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,12 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie').split(':')[1].trim() === 'true';
+    //  const isLoggedIn = req.get('Cookie').split(':')[1].trim();
     return res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     })
 }
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn:true');
+    res.setHeader('Set-Cookie', 'loggedIn:true; HttpOnly');
     res.redirect('/');
 }
