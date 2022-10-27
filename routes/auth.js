d4c96a77a5532c707a3fb122c674ed62495e866e
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-10-27 14:18:25 +0700

    Built-In & Custom Validator

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,14 @@ router.post('/login', login.postLogin);
 router.post('/logout', login.postLogout);
 
 router.get('/signup', login.getSignUp);
-router.post('/signup', check('email').isEmail().withMessage("Please enter a valid email"), login.postSignUp);
+router.post('/signup', check('email')
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .custom((value, { req }) => {
+        if (value === 'a@a.com') {
+            throw new Error('This email address if forbidden');
+        }
+        return true;
+    }), login.postSignUp);
 
 module.exports = router;
