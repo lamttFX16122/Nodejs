48ff92b5511dd9844d5f531bed1b303c6960638f
Thanh Lam <lamttFX16122@funix.edu.vn>
2022-10-28 22:29:23 +0700

    Restricting File Access

--- controllers/shop.js
+++ controllers/shop.js
@@ -183,16 +183,27 @@ exports.cartDeleteItem = (req, res, next) => {
 }
 
 //Get invoice
-exports.getInvoice = async(req, res, next) => {
+exports.getInvoice = (req, res, next) => {
     const orderId = req.params.orderId;
-    const invoiceName = 'invoice-' + orderId + '.pdf';
-    const invoicePath = path.join('data', 'invoices', invoiceName);
-    await fs.readFile(invoicePath, (err, data) => {
-        if (err) {
-            next(err);
-        }
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', `inline; filename=${invoiceName}`);
-        res.send(data);
-    })
+    Order.findById(orderId)
+        .then(order => {
+            if (!order) {
+                return next(new Error('No order found'));
+            }
+            if (order.user.userId.toString() !== req.user._id.toString()) {
+                return next(new Error('Unauthorized'));
+            }
+            const invoiceName = 'invoice-' + orderId + '.pdf';
+            const invoicePath = path.join('data', 'invoices', invoiceName);
+            fs.readFile(invoicePath, (err, data) => {
+                if (err) {
+                    next(err);
+                }
+                res.setHeader('Content-Type', 'application/pdf');
+                res.setHeader('Content-Disposition', `inline; filename=${invoiceName}`);
+                res.send(data);
+            })
+        })
+        .catch(err => next(err));
+
 }
